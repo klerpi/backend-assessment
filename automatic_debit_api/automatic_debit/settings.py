5efeff267033e1ae1eb61db12b89ecdd2ef8cc54
Daniel Andrade <6894717+klerpi@users.noreply.github.com>
2021-01-16 10:54:12 -0300

    Add api response pagination of 10 entries per page

--- automatic_debit_api/automatic_debit/settings.py
+++ automatic_debit_api/automatic_debit/settings.py
@@ -110,3 +110,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+
+# Django Rest Framework
+# https://www.django-rest-framework.org/
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}

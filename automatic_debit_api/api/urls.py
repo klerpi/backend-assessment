1af65e8612fa0f4aa0c78f5edaf2142e04ad52d8
Daniel Andrade <6894717+klerpi@users.noreply.github.com>
2021-01-17 16:15:00 -0300

    Add view to issue activation of Product

--- automatic_debit_api/api/urls.py
+++ automatic_debit_api/api/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 
-from .views import ProductListAPIView, ProductDetailAPIView
+from .views import (
+    ProductListAPIView,
+    ProductDetailAPIView,
+    ProductActivationRequestAPIView,
+)
 
 urlpatterns = [
     path("products/", ProductListAPIView.as_view(), name="product_list"),
     path("products/<int:pk>/", ProductDetailAPIView.as_view(), name="product_detail"),
+    path(
+        "products/<int:pk>/activate/",
+        ProductActivationRequestAPIView.as_view(),
+        name="product_activate",
+    ),
 ]
--- automatic_debit_api/api/views.py
+++ automatic_debit_api/api/views.py
@@ -1,11 +1,12 @@
-from rest_framework import generics
+from rest_framework import generics, status, views
+from rest_framework.response import Response
 from django.contrib.auth.models import User
 
 from .models import Product
 from .serializers import ProductSerializer
 
 
-# Helper function
+# Helper functions
 def get_full_queryset_if_superuser(user):
     """
     Returns the full queryset if the current user is a superuser
@@ -15,6 +16,26 @@ def get_full_queryset_if_superuser(user):
         return Product.objects.all()
     else:
         return Product.objects.filter(user=user)
+
+
+def toggle_activation_issued_if_author_or_superuser(user, product, new_value):
+    """
+    Sets the activation_issued property to new_value and returns the Response object
+    Property is only set if the current user is the author of the product or a superuser
+    Otherwise an error message is sent instead
+    """
+    if user == product.user or user.is_superuser:
+        product.activation_issued = new_value
+        product.save()
+        serializer = ProductSerializer(instance=product)
+        return Response(serializer.data)
+    else:
+        return Response(
+            {
+                "message": "You need to be the author of the product or a superuser to do this"
+            },
+            status=status.HTTP_403_FORBIDDEN,
+        )
 
 
 class ProductListAPIView(generics.ListCreateAPIView):
@@ -37,3 +58,15 @@ class ProductDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
 
     def get_queryset(self):
         return get_full_queryset_if_superuser(self.request.user)
+
+
+class ProductActivationRequestAPIView(views.APIView):
+    def post(self, request, pk=None):
+        """
+        Sets the activation_issued property to True in a Product instance
+        The caller is only allowed to set it if it's the creator or if it's a superuser
+        """
+        product = generics.get_object_or_404(Product, pk=pk)
+        return toggle_activation_issued_if_author_or_superuser(
+            request.user, product, new_value=True
+        )

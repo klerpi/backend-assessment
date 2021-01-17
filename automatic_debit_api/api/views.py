50ab60fc0b373686e443a8c045a2af5edfe2749e
Daniel Andrade <6894717+klerpi@users.noreply.github.com>
2021-01-17 13:52:47 -0300

    Add Product list & create view

--- automatic_debit_api/api/views.py
+++ automatic_debit_api/api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth.models import User
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductListAPIView(generics.ListCreateAPIView):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        current_user = self.request.user
+        if current_user.is_superuser:
+            return Product.objects.all()
+        else:
+            return Product.objects.filter(user=current_user)
+
+    def perform_create(self, serializer):
+        user_pk = self.request.user.id
+        user = generics.get_object_or_404(User, pk=user_pk)
+        serializer.save(user=user)

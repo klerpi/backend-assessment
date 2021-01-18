7b6d46e9d55df4f41ee43e39c0b8be4ed4bf70dc
Daniel Andrade <6894717+klerpi@users.noreply.github.com>
2021-01-18 08:46:53 -0300

    Add django-probes to installed apps

--- automatic_debit_api/automatic_debit/settings.py
+++ automatic_debit_api/automatic_debit/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # External apps
+    "django_probes",
     "rest_framework",
     # Internal apps
     "api",

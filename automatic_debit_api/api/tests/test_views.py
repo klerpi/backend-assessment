5c5a478238dfec64f0f4919e5bb7a460d7df86ce
Daniel Andrade <6894717+klerpi@users.noreply.github.com>
2021-01-17 22:40:46 -0300

    Add tests for activation and cancelation routes

--- automatic_debit_api/api/tests/test_views.py
+++ automatic_debit_api/api/tests/test_views.py
@@ -103,3 +103,40 @@ class ProductDetailAPITestCase(APITestCase):
 
         self.assertEqual(delete_resp.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(get_resp.status_code, status.HTTP_404_NOT_FOUND)
+
+
+class ProductActivationAndCancelationAPITestCase(APITestCase):
+    def setUp(self):
+        user = User.objects.create_user("test", "test@python.com", "aGoodPass24")
+        self.client.force_authenticate(user=user)  # Default logged in user
+
+        product_attributes = {
+            "user": user,
+            "title": "Example Product",
+            "notification_email": "test@product.com",
+        }
+        self.product = Product.objects.create(**product_attributes)
+
+    def test_activation_of_a_product(self):
+        get_resp = self.client.get(
+            reverse("product_activate", kwargs={"pk": self.product.id})
+        )
+        post_resp = self.client.post(
+            reverse("product_activate", kwargs={"pk": self.product.id})
+        )
+
+        self.assertEqual(get_resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        self.assertEqual(post_resp.status_code, status.HTTP_200_OK)
+        self.assertTrue(post_resp.data["activation_issued"])
+
+    def test_cancelation_of_a_product(self):
+        get_resp = self.client.get(
+            reverse("product_cancel", kwargs={"pk": self.product.id})
+        )
+        post_resp = self.client.post(
+            reverse("product_cancel", kwargs={"pk": self.product.id})
+        )
+
+        self.assertEqual(get_resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        self.assertEqual(post_resp.status_code, status.HTTP_200_OK)
+        self.assertFalse(post_resp.data["activation_issued"])

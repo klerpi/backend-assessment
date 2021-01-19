b64368dc897dd3e8bb8c68d655773e3205acc71d
Daniel Andrade <6894717+klerpi@users.noreply.github.com>
2021-01-19 15:16:19 -0300

    Add locust task for post in /products/

--- stress-test/main.py
+++ stress-test/main.py
@@ -1,4 +1,6 @@
 import os
+import string
+import random
 from locust import HttpUser, task
 
 
@@ -17,6 +19,34 @@ class WebsiteUser(HttpUser):
             token = response.json()["access"]
             self.auth_header = {"Authorization": "Bearer " + token}
 
+    def get_random_string(self):
+        """
+        Generates a string with 10 random letters
+        """
+        letters = string.ascii_letters
+        return "".join(random.choice(letters) for _ in range(10))
+
+    def get_random_email(self):
+        """
+        Generates a randomized email
+        """
+        user = self.get_random_string()
+        domain = self.get_random_string()
+        return f"{user}@{domain}.com"
+
     @task
     def get_products(self):
         self.client.get(url="/products/", headers=self.auth_header)
+
+    @task
+    def new_product(self):
+        data = {
+            "title": self.get_random_string(),
+            "notification_email": self.get_random_email(),
+        }
+
+        self.client.post(
+            url="/products/",
+            json=data,
+            headers=self.auth_header,
+        )

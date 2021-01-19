4afb21c1bd7bae1822da292b98e85bc294838426
Daniel Andrade <6894717+klerpi@users.noreply.github.com>
2021-01-19 14:35:14 -0300

    Add locust task for /products/

--- stress-test/main.py
+++ stress-test/main.py
@@ -1,1 +1,18 @@
-print("Hello, world!")
+import os
+from locust import HttpUser, task
+
+
+class WebsiteUser(HttpUser):
+    def on_start(self):
+        credentials = {
+            "username": os.environ.get("DJANGO_SUPERUSER_USERNAME"),
+            "password": os.environ.get("DJANGO_SUPERUSER_PASSWORD"),
+        }
+
+        with self.client.post("/token/", json=credentials) as response:
+            token = response.json()["access"]
+            self.auth_header = {"Authorization": "Bearer " + token}
+
+    @task
+    def get_products(self):
+        self.client.get(url="/products/", headers=self.auth_header)
